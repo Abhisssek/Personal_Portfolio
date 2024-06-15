3beb7d7da77fafc4e7312b6a688a98593f8e206d
Abhisek <abhisekpattanayak07@gmail.com>
2024-06-15 10:16:14 +0530

    commit

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,11 @@
 // routes/contact.js
 const express = require('express');
+const { createContact } = require('../controllers/Controller');
 const router = express.Router();
-const  createContact  = require('../controllers/Controller');
+console.log('Current directory:', __dirname);
+console.log('Resolved path:', require('path').resolve(__dirname, '../controllers/Controller'));
+const Controller = require(require('path').resolve(__dirname, '../controllers/Controller'));
+
 
 router.post('/create', createContact);
 
